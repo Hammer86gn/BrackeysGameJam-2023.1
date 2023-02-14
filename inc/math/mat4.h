0b39542374772b003ce1b7854433affb4b43f3c8
Chloe <56194759+Hammer86gn@users.noreply.github.com>
2023-02-14 18:41:13 -0500

    feat: Mat4 rotations

--- inc/math/mat4.h
+++ inc/math/mat4.h
@@ -3,6 +3,8 @@
 
 #define __MAT4_FLOAT_SIZE__ 16
 
+#include <math/vec3.h>
+
 typedef float* mat4;
 
 mat4 mat4_create_default( float default_value );
@@ -11,5 +13,8 @@ mat4 mat4_add( mat4 a, mat4 b );
 mat4 mat4_multiply( mat4 a, mat4 b );
 
 mat4 mat4_subtract( mat4 a, mat4 b );
+
+void mat4_rotate( mat4* matrix, float radians, struct vec3 axis );
+void mat4_translate( mat4* matrix, struct vec3 translation );
 
 #endif //BRACKEYSGAMEJAM_MAT4_H
--- src/mat4.c
+++ src/mat4.c
@@ -1,5 +1,6 @@
 #include <math/mat4.h>
 #include <malloc.h>
+#include <math.h>
 
 mat4 mat4_create_default( float default_value )
 {
@@ -41,4 +42,58 @@ mat4 mat4_subtract( const mat4 a, const mat4 b )
         resultant[i] = a[i] - b[i];
     }
     return resultant;
+}
+
+void mat4_rotate( mat4* matrix, float radians, struct vec3 axis ) // I have zero idea if this works
+{
+    float angle_cosine = cosf( radians );
+    float angle_sine = sinf( radians );
+
+    struct vec3 normalized_axis = normalize( axis );
+
+    struct vec3 temp_scalar = {
+            .x = 1 - angle_cosine,
+            .y = 1 - angle_cosine,
+            .z = 1 - angle_cosine
+    };
+
+    struct vec3 rotational_axis = vec3_multiply(temp_scalar, normalized_axis);
+
+    mat4 resultant = malloc( __MAT4_FLOAT_SIZE__ * sizeof( float ) );
+
+    float x = rotational_axis.x;
+    float y = rotational_axis.y;
+    float z = rotational_axis.z;
+
+    resultant[0] = angle_cosine + x * x * temp_scalar.x;
+    resultant[1] = x * y * temp_scalar.x + z * angle_sine;
+    resultant[2] = x * z * temp_scalar.x - y * angle_sine;
+    resultant[3] = 0;
+
+    resultant[4] = x * y * temp_scalar.x - z * angle_sine;
+    resultant[5] = angle_cosine + y * y * temp_scalar.y;
+    resultant[6] = y * z * temp_scalar.y + x * angle_sine;
+    resultant[7] = 0;
+
+    resultant[8] = x * z * temp_scalar.x + y * angle_sine;
+    resultant[9] = y * z * temp_scalar.y - x * angle_sine;
+    resultant[10] = angle_cosine + z * z * temp_scalar.z;
+    resultant[11] = 0;
+
+    resultant[12] = 0;
+    resultant[13] = 0;
+    resultant[14] = 0;
+    resultant[15] = 1;
+
+    mat4_multiply(*matrix, resultant);
+    free(resultant);
+}
+
+void mat4_translate( mat4* matrix, struct vec3 translation )
+{
+    mat4 deref = *matrix;
+    deref[12] += translation.x;
+    deref[13] += translation.y;
+    deref[14] += translation.z;
+    matrix = &deref;
 }
